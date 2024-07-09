fa63fefb9a7ed92af7723fd750e4fead43f48aed
scq <140981824+sc4sov@users.noreply.github.com>
2024-07-10 01:43:25 +0300

    Add files via upload

--- moon_bot.lua
+++ moon_bot.lua
@@ -1,3 +1,7 @@
+script_name("moonbot")
+script_version("1.1")
+script_authors("scq", "oixro")
+
 local sampev = require "lib.samp.events"
 local inicfg = require 'inicfg'
 local ffi = require 'ffi'
@@ -71,12 +75,48 @@ ffi.cdef [[
 if not doesDirectoryExist(getWorkingDirectory().."\\config") then createDirectory(getWorkingDirectory().."\\config") end
 inicfg.save(ini, iniFile)
 
+function update()
+    local raw = 'https://raw.githubusercontent.com/sc4sov/moonbot/main/update.json'
+    local dlstatus = require('moonloader').download_status
+    local requests = require('requests')
+    local f = {}
+    function f:getLastVersion()
+        local response = requests.get(raw)
+        if response.status_code == 200 then
+            return decodeJson(response.text)['last']
+        else
+            return 'UNKNOWN'
+        end
+    end
+    function f:download()
+        local response = requests.get(raw)
+        if response.status_code == 200 then
+            downloadUrlToFile(decodeJson(response.text)['url'], thisScript().path, function (id, status, p1, p2)
+                if status == dlstatus.STATUSEX_ENDDOWNLOAD then
+                    chat_message("Обновление скрипта завершено")
+                    if not script.find('ML%-AutoReboot') then thisScript():reload() end
+                end
+            end)
+        else
+			chat_message(string.format("Ошибка при обновлении скрипта. Код: {ffdead}#%d", response.status_code))
+        end
+    end
+    return f
+end
+
 function main() 
     if not isSampfuncsLoaded() or not isSampLoaded() then return end 
     while not isSampAvailable() do wait(100) end
-
+    local lastver = update():getLastVersion()
+    if tonumber(thisScript().version) < tonumber(lastver) then
+		chat_message(string.format("Сейчас произойдет обновление скрипта до версии {ffdead}%s (Текущая версия: %s)", lastver, thisScript().version))
+        update():download()
+        return
+    end
     chat_message('Скрипт готов к работе. Команда: {FFDEAD}/a.info{ffffff}')
-
+    if tonumber(thisScript().version) > tonumber(lastver) then
+        chat_message(string.format("У вас установлена developer версия: {ffdead}%s",  thisScript().version))
+    end
     bot = mb.add(ini.cfg.bot_nickname)
     if ini.cfg.bot_debug then chat_message(string.format('{FFDEAD}%s{ffffff}: Бот автоматически добавлен', bot.name)) end
 
